b4a882703a42815652442900671d842e1ee15835
Mikołaj Lubiak <lubiak@proton.me>
2023-09-04 11:41:43 +0200

    changed the compile.sh file
    Signed-off-by: Mikołaj Lubiak <lubiak@proton.me>

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-clang++ main.cpp Game.cpp TextureManager.cpp -I/usr/local/include/SDL2 -L/usr/local/lib -lSDL2 -lSDL2_image
+clang++ main.cpp Game.cpp TextureManager.cpp -lSDL2 -lSDL2_image -Ofast -march=native -mtune=native
