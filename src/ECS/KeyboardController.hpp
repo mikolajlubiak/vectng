54fbc193642d017e5c029fd62de7697ba79447c9
Mikołaj Lubiak <lubiak@proton.me>
2023-09-04 11:41:44 +0200

    Simplified KeyboardController
    Signed-off-by: Mikołaj Lubiak <lubiak@proton.me>

--- src/ECS/KeyboardController.hpp
+++ src/ECS/KeyboardController.hpp
@@ -17,45 +17,37 @@ public:
 		while(SDL_PollEvent(&Game::event)) {
 			if (Game::event.type == SDL_KEYDOWN) {
 				switch (Game::event.key.keysym.sym) {
-				case SDLK_w:
-					transform->velocity.y = -1;
-					break;
-				case SDLK_s:
-					transform->velocity.y = 1;
-					break;
-				case SDLK_a:
-					transform->velocity.x = -1;
-					break;
-				case SDLK_d:
-					transform->velocity.x = 1;
-					break;
-				default: break;
+					case SDLK_w:
+						transform->velocity.y = -1;
+						break;
+					case SDLK_s:
+						transform->velocity.y = 1;
+						break;
+					case SDLK_a:
+						transform->velocity.x = -1;
+						break;
+					case SDLK_d:
+						transform->velocity.x = 1;
+						break;
+					default: break;
 				}
 			}
-			if (Game::event.type == SDL_KEYUP) {
+			else if (Game::event.type == SDL_KEYUP) {
 				switch (Game::event.key.keysym.sym) {
-				case SDLK_w:
-					transform->velocity.y = 0;
-					break;
-				case SDLK_s:
-					transform->velocity.y = 0;
-					break;
-				case SDLK_a:
-					transform->velocity.x = 0;
-					break;
-				case SDLK_d:
-					transform->velocity.x = 0;
-					break;
-				default: break;
+					case SDLK_w:
+					case SDLK_s:
+						transform->velocity.y = 0;
+						break;
+					case SDLK_a:
+					case SDLK_d:
+						transform->velocity.x = 0;
+						break;
+					default: break;
 				}
 			}
-			switch (Game::event.type) {
-			case SDL_QUIT:
+			else if (Game::event.type == SDL_QUIT) {
 				SDL_Quit();
 				std::cout << "Game Cleaned!" << std::endl;
-				break;
-			default:
-				break;
 			}
 
 		}
