46adf3d0de4c74cf8a52d86c9f2e669cce4e44f5
Mikołaj Lubiak <lubiak@proton.me>
2024-09-04 17:56:13 +0200

    Simplify `==` and `!=` operator overloaders for `Vector2D`

--- src/Vector2D.cpp
+++ src/Vector2D.cpp
@@ -90,17 +90,7 @@ std::ostream &operator<<(std::ostream &stream, const Vector2D &vec) {
 }
 
 bool operator==(const Vector2D &v1, const Vector2D &v2) {
-  if (v1.x == v2.x && v1.y == v2.y) {
-    return true;
-  } else {
-    return false;
-  }
+  return v1.x == v2.x && v1.y == v2.y;
 }
 
-bool operator!=(const Vector2D &v1, const Vector2D &v2) {
-  if (v1 == v2) {
-    return false;
-  } else {
-    return true;
-  }
-}
+bool operator!=(const Vector2D &v1, const Vector2D &v2) { return !(v1 == v2); }
